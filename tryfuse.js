15f06f8231b9e58c11a67145fc49cdec14dd28c9
ameba23 <ameba23@systemli.org>
2021-10-31 20:45:04 +0100

    Add fuse handlers for reading files

--- tryfuse.js
+++ tryfuse.js
@@ -1,37 +1,59 @@
 #!/usr/bin/env node
 const Fuse = require('fuse-native')
 const fs = require('fs')
-const path = require('path')
+const { join } = require('path')
+const log = console.log
 
 const basepath = './test'
 
 const handlers = {
-  getattr: function (p, cb) {
-    console.log('getattr(%s)', p)
-    fs.stat(path.join(basepath, p), (err, stat) => {
-      console.log(err)
+  getattr (p, cb) {
+    log('getattr(%s)', p)
+    fs.stat(join(basepath, p), (err, stat) => {
       if (err) return cb(err.errno || Fuse.ENOENT)
       cb(0, stat)
     })
   },
-  readdir: function (p, cb) {
-    console.log('readdir', p)
-    fs.readdir(path.join(basepath, p), (err, files) => {
+  readdir (p, cb) {
+    log('readdir', p)
+    fs.readdir(join(basepath, p), (err, files) => {
       if (err) return cb(err.errno || Fuse.ENOENT)
       cb(0, files)
+    })
+  },
+  open (path, flags, cb) {
+    log('open', path, flags)
+    fs.open(join(basepath, path), flags, (err, fd) => {
+      console.log(err, fd)
+      if (err) return cb(err.errno)
+      cb(0, fd)
+    })
+  },
+  read (path, fd, buf, len, pos, cib) {
+    log('read', path, fd, len, pos, buf.length)
+    fs.read(fd, buf, 0, len, pos, (err, bytesRead) => {
+      if (err) return cib(err.errno)
+      console.log(err, bytesRead, buf.length, buf.toString())
+      cib(bytesRead)
+    })
+  },
+  release (path, fd, cb) {
+    log('release', path, fd)
+    fs.close(fd, (err) => {
+      cb(err ? err.errno : 0)
     })
   }
 }
 
-const f = new Fuse(process.argv[2] || './mnt', handlers, { autoCache: true })
+const fuse = new Fuse(process.argv[2] || './mnt', handlers, { autoCache: true })
 
-f.mount(function (err) {
-  console.log(err)
-  console.log('mounted ...')
+fuse.mount(function (err) {
+  log(err)
+  log('mounted ...')
 
   process.once('SIGINT', function () {
-    f.unmount(function (err) {
-      console.log('unmounted', err)
+    fuse.unmount(function (err) {
+      log('unmounted', err)
       process.exit()
     })
   })
