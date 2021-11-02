1a8a468c343dc976fcea0fbcaf638f2cb1e9da3d
ameba23 <ameba23@systemli.org>
2021-11-02 21:28:55 +0100

    Test for stat messages

--- index.js
+++ index.js
@@ -28,21 +28,33 @@ class Hdp extends EventEmitter {
       this.emit('connection')
     })
     this.shares = options.shares || []
-    this.basePath = this.shares[0] // temp
   }
 
-  async connect (name) {
-    log('Joining ', name)
+  async join (name) {
+    log(`Joining ${name}`)
     const discovery = this.hyperswarm.join(nameToTopic(name), { server: true, client: true })
     // await discovery.flushed() // Waits for the topic to be fully announced on the DHT
 
     // await this.swarm.flush() // Waits for the swarm to connect to pending peers.
   }
 
+  async leave (name) {
+    log(`Leaving ${name}`)
+    this.hyperswarm.leave(nameToTopic(name))
+  }
+
   onReadDir(path, remotePk) {
     // Read local dir, respond to peer
     const self = this
-    fs.readdir(join(this.basePath, path), (err, files) => {
+    if (!this.shares.length) {
+      if (path === '/' || path === '') {
+        self.peers[remotePk].respondReadDir(err, [], path)
+      } else {
+        // ENOENT
+        self.peers[remotePk].respondReadDir({ errno: -2 }, undefined, path)
+      }
+    }
+    fs.readdir(join(this.shares[0], path), (err, files) => {
       self.peers[remotePk].respondReadDir(err, files, path)
     })
   }
@@ -50,7 +62,7 @@ class Hdp extends EventEmitter {
   onStat(path, remotePk) {
     // Read local dir, respond to peer
     const self = this
-    fs.stat(join(this.basePath, path), (err, stat) => {
+    fs.stat(join(this.shares[0], path), (err, stat) => {
       self.peers[remotePk].respondStat(err, stat, path)
     })
   }
@@ -67,7 +79,7 @@ class Hdp extends EventEmitter {
     if (Object.keys(this.peerNames).includes(path[0])) {
       return this.peerNames[path[0]].readDir(path.slice(1).join('/'))
     }
-    throw new Error('no file')
+    throw createError(-2)
   }
 
   async stat (pathString) {
@@ -79,6 +91,6 @@ class Hdp extends EventEmitter {
       return this.peerNames[path[0]].stat(path.slice(1).join('/'))
     }
     console.log(path)
-    throw new Error('no file')
+    throw createError(-2)
   }
 }
--- test/index.test.js
+++ test/index.test.js
@@ -11,30 +11,37 @@ describe('basic', (context) => {
   })
 
   context('basic', async (assert) => {
-
     hdp1 = Hdp({ shares: [resolvePath('./alice-files')] })
     hdp2 = Hdp({ shares: [resolvePath('./bob-files')] })
 
     const swarmName = 'some place'
 
-    await hdp1.connect(swarmName)
+
+    await hdp1.join(swarmName)
+    await hdp2.join(swarmName)
     await new Promise((resolve, reject) => {
-      setTimeout(resolve, 500)
-    })
-    await hdp2.connect(swarmName)
-    await new Promise((resolve, reject) => {
-      hdp1.on('connection', () => {
-        resolve()
-      })
+      hdp1.once('connection', resolve)
     })
     const files = await hdp1.readDir('/')
-    console.log(files)
+    // console.log(files)
     assert.equals(files.length, 1, 'One directory')
     const rfiles = await hdp1.readDir(files[0])
-    console.log('rfiles', rfiles)
+    // console.log('rfiles', rfiles)
     assert.equals(rfiles.length, 1, 'One file')
+
+    hdp1.readDir('/not-a-peer').catch((err) => {
+      assert.true(err, 'Should give error on reading non-existant directory')
+    })
+
     const stat = await hdp1.stat(`${files[0]}/${rfiles[0]}`)
     console.log('output', stat)
+    assert.true(typeof stat.size === 'number', 'Stat object has a size property')
+
+    hdp1.stat(`${files[0]}/not-a-file`).catch((err) => {
+      console.log('err', err)
+      assert.true(err.errno === -2)
+    })
+
     await hdp1.hyperswarm.destroy()
     await hdp2.hyperswarm.destroy()
   })
